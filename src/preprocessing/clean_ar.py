cbd8da290a18fba7ed896a1af754c930affa50be
MEDDOUR Cherif <meddourcherif1981@gmail.com>
2025-12-25 12:11:20 +0100

    Update clean_ar.py

--- src/preprocessing/clean_ar.py
+++ src/preprocessing/clean_ar.py
@@ -1,1 +1,10 @@
+import re
 
+def clean_arabic(text):
+    text = re.sub(r"[إأآا]", "ا", text)
+    text = re.sub(r"ى", "ي", text)
+    text = re.sub(r"ؤ", "و", text)
+    text = re.sub(r"ئ", "ي", text)
+    text = re.sub(r"ة", "ه", text)
+    text = re.sub(r"[^\u0600-\u06FF\s]", "", text)
+    return text.strip()
